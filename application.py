662ef5f133c584f81a78c227fcf956c5e6cf5bbd
Anton Kotov <akotov@microsoft.com>
2018-08-26 00:36:31 -0700

    Implemented basic HTML templates, Registration, Login

--- application.py
+++ application.py
@@ -1,17 +1,32 @@
 import os
 
-from flask import Flask, session
+from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
+from tempfile import mkdtemp
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
+from werkzeug.exceptions import default_exceptions
+from werkzeug.security import check_password_hash, generate_password_hash
 
+from helpers import apology, login_required
+
+# Configure application
 app = Flask(__name__)
 
 # Check for environment variable
 if not os.getenv("DATABASE_URL"):
     raise RuntimeError("DATABASE_URL is not set")
 
+# Import from CS50 PSET7 Finance app: Ensure responses aren't cached.
+@app.after_request
+def after_request(response):
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
 # Configure session to use filesystem
+app.config["SESSION_FILE_DIR"] = mkdtemp()
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
@@ -22,5 +37,119 @@ db = scoped_session(sessionmaker(bind=engine))
 
 
 @app.route("/")
+@login_required
 def index():
     return "Project 1: TODO"
+
+
+# Import from CS50 PSET7 Finance
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    """Register user"""
+
+    # Forget any user_id
+    session.clear()
+
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+        # Extracting username and password
+        username = request.form.get("username")
+        password = request.form.get("password")
+        confirmation = request.form.get("confirmation")
+
+        # Ensure username was submitted
+        if not username:
+            return apology("must provide username")
+
+        # Ensure password was submitted
+        elif not password:
+            return apology("must provide password")
+
+        # Ensure confirmation was submitted too
+        elif not confirmation:
+            return apology("must confirm password")
+
+        # Ensure passwords matches
+        elif password != confirmation:
+            return apology("confirmation doesn't match")
+
+        # Ensure username wasn't used already
+        userid = db.execute("SELECT id FROM users WHERE username = :username",
+                          {"username": username}).fetchone()
+
+        if userid:
+            return apology("try another username")    
+        
+        # Add user to users database
+        db.execute("INSERT INTO users (username, hash) VALUES (:username, :pwdhash)",
+                   {"username": username,
+                    "pwdhash": generate_password_hash(password)})
+        
+        db.commit()
+
+        # Checking new user id
+        userid = db.execute("SELECT id FROM users WHERE username = :username",
+                            {"username": username}).fetchone()
+
+        # Remember which user has logged in
+        session["user_id"] = userid[0]
+
+        # Redirect user to home page
+        return redirect("/")
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("register.html")
+
+
+# Import from CS50 PSET7 Finance
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    """Log user in"""
+
+    # Forget any user_id
+    session.clear()
+
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+
+        username = request.form.get("username")
+        password = request.form.get("password")
+        
+        # Ensure username was submitted
+        if not username:
+            return apology("must provide username")
+
+        # Ensure password was submitted
+        elif not password:
+            return apology("must provide password")
+
+        # Query database for username
+        user = db.execute("SELECT * FROM users WHERE username = :username",
+                         {"username": username}).fetchone()
+
+        # Ensure username exists and password is correct
+        if not user or not check_password_hash(user.hash, password):
+            return apology("invalid username and/or password")
+
+        # Remember which user has logged in
+        session["user_id"] = user.id
+
+        # Redirect user to home page
+        return redirect("/")
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("login.html")
+
+
+# Import from CS50 PSET7 Finance
+@app.route("/logout")
+def logout():
+    """Log user out"""
+
+    # Forget any user_id
+    session.clear()
+
+    # Redirect user to login form
+    return redirect("/")
